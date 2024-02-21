d56976d3e5029f04aade06b76015fd18ce7cbf76
imilay11 <imilay11@gmail.com>
2024-02-21 21:47:22 +0200

    Added freelance meta

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,7 +9,9 @@ const Footer: FC<FooterProps> = () => {
   return (
     <footer className="footer border-t z-10 border-t-[#33353F] text-white">
       <div className="container p-12 flex justify-between m-auto">
-        <span className="z-10">LOGO</span>
+        <a href="https://freelancehunt.com/freelancer/IgorMilya.html?from=shield&r=vYkGO" target="_blank"><img
+          src="https://freelancehunt.com/shields/display/id/989321/type/reviews?style=flat&amp;lang=ru"
+          alt="Freelancehunt — простой и честный фриланс" /></a>
         <p className="text-slate-600">All rights reserved</p>
       </div>
     </footer>
