d4891f33bdf51960042f6b7bd487ba9dbe6fc82c
Ihor <78513411+IgorMilya@users.noreply.github.com>
2025-01-03 19:57:25 +0200

    Update projectsSection.utils.ts

--- src/components/projects-section/projectsSection.utils.ts
+++ src/components/projects-section/projectsSection.utils.ts
@@ -20,6 +20,14 @@ export const tabCardData: TabCardData[] = [
     previewUrl: 'https://quiz-sigma-pied-62.vercel.app',
   },
   {
+    title: 'TODO-List',
+    description: 'Implemented user login using Firebase. Also, I used Rust for compiling and tailwind. After it toggles to the server call is made to check the user's permissions. All removed items is grayed out and be seen only when the toggler is on.',
+    image: 'images/quiz.png',
+    tag: ['all', 'web'],
+    gitUrl: 'https://github.com/IgorMilya/TODO-List',
+    previewUrl: 'https://todo-list-puce-iota.vercel.app',
+  },
+  {
     title: 'Next.js Portfolio Website',
     description: 'Responsive website',
     image: 'images/portfolio.png',
